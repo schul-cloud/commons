5041f4e34fda88c7621d9de92bc8f778efe6c61c
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-11-22 15:25:33 +0100

    linting

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,19 +1,19 @@
 import { IConfiguration } from '@/interfaces/IConfiguration';
 
 class Configuration implements IConfiguration {
+  get(key: string) {
+    throw new Error('Method not implemented.');
+  }
 
-	get(key: string) {
-		throw new Error('Method not implemented.');
-	}
-	has(key: string): boolean {
-		throw new Error('Method not implemented.');
-	}
-	set(key: string, value: any): void {
-		throw new Error('Method not implemented.');
-	}
-	isValid(): boolean {
-		throw new Error('Method not implemented.');
-	}
+  has(key: string): boolean {
+    throw new Error('Method not implemented.');
+  }
 
+  set(key: string, value: any): void {
+    throw new Error('Method not implemented.');
+  }
 
+  isValid(): boolean {
+    throw new Error('Method not implemented.');
+  }
 }
