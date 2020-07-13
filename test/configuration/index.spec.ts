16d180b81ad25b677372ee39d33df28118ffeada
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-07-13 15:03:46 +0200

    fix counts due to an added value

--- test/configuration/index.spec.ts
+++ test/configuration/index.spec.ts
@@ -306,7 +306,7 @@ describe('test configuration', () => {
 				configDir: 'test/data'
 			});
 			const before = config.toObject();
-			expect(Object.keys(before).length).to.be.equal(7);
+			expect(Object.keys(before).length).to.be.equal(8);
 			expect(before['Domain']).to.be.equal('localhost');
 			config.set('Domain', 'otherdomain.tld');
 			expect(config.get('Domain')).to.be.equal('otherdomain.tld');
@@ -319,7 +319,7 @@ describe('test configuration', () => {
 				configDir: 'test/data'
 			});
 			const before = config.toObject();
-			expect(Object.keys(before).length).to.be.equal(7);
+			expect(Object.keys(before).length).to.be.equal(8);
 			expect('String' in before).to.be.false;
 			expect(() => config.get('String')).to.throw;
 			config.set('String', 'newValueNotDefinedBefore');
